6025c367de9cacd2bb8936ce6912420ababa7f65
SunnySatwik <sunny.satwik10@gmail.com>
2024-11-18 21:45:57 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaButton = document.querySelector('.cta');
+    ctaButton.addEventListener('click', () => {
+        alert('Welcome to NH-Verse!');
+    });
+});
